bf1c22945ee0f6fc7b6593854a5ff361e8e086c7
Elmatador <kmouad29@gmail.com>
2020-08-27 19:24:42 -0600

    fixing service-worker to have the app working while offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,64 +10,138 @@ const FILES_TO_CACHE = [
   "/service-worker.js",
 ];
 
-const STATIC_CACHE = "static-cache-v1";
-const RUNTIME_CACHE = "runtime-cache";
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches
-      .open(STATIC_CACHE)
-      .then((cache) => cache.addAll(FILES_TO_CACHE))
-      .then(() => self.skipWaiting())
+// registers service worker
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+
+  self.skipWaiting();
 });
 
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener("activate", (event) => {
-  const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
-  event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) => {
-        // return array of cache names that are old to delete
-        return cacheNames.filter(
-          (cacheName) => !currentCaches.includes(cacheName)
-        );
-      })
-      .then((cachesToDelete) => {
-        return Promise.all(
-          cachesToDelete.map((cacheToDelete) => {
-            return caches.delete(cacheToDelete);
-          })
-        );
-      })
-      .then(() => self.clients.claim())
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
+
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
-  // non GET requests are not cached and requests to other origins are not cached
-  if (
-    event.request.method !== "GET" ||
-    !event.request.url.startsWith(self.location.origin)
-  ) {
-    event.respondWith(fetch(event.request));
-    return;
-  }
-
-  // handle runtime GET requests for data from /api routes
-  if (event.request.url.includes("/api/images")) {
-    // make network request and fallback to cache if network request fails (offline)
-    event.respondWith(
-      caches.open(RUNTIME_CACHE).then((cache) => {
-        return fetch(event.request)
+// cache responses for requests for data - opens the data cache
+self.addEventListener("fetch", function (evt) {
+  if (evt.request.url.includes("/api")) {
+    console.log("[Service Worker] Fetch (data)", evt.request.url);
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then((cache) => {
+        return fetch(evt.request)
           .then((response) => {
-            cache.put(event.request, response.clone());
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
             return response;
           })
-          .catch(() => caches.match(event.request));
+          .catch((err) => {
+            return cache.match(evt.request);
+          });
       })
     );
     return;
   }
+  evt.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(evt.request).then((response) => {
+        return response || fetch(evt.request);
+      });
+    })
+  );
 });
+
+// const FILES_TO_CACHE = [
+//   "/",
+//   "/index.html",
+//   "/index.js",
+//   "/icons/icon-512x512.png",
+//   "/icons/icon-192x192.png",
+//   "/db.js",
+//   "/manifest.json",
+//   "/styles.css",
+//   "/service-worker.js",
+// ];
+
+// const CACHE_NAME = "static-cache";
+// const DATA_CACHE_NAME = "data-cache";
+
+// self.addEventListener("install", function (ev) {
+//   ev.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => {
+//       console.log("Pre-cache successful");
+//       return cache.addAll(TO_CACHE);
+//     })
+//   );
+
+//   self.skipWaiting();
+// });
+
+// self.addEventListener("activate", function (ev) {
+//   ev.waitUntil(
+//     caches.keys().then((keyList) => {
+//       return Promise.all(
+//         keyList.map((key) => {
+//           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+//             console.log("Removing old cache data", key);
+//             return caches.delete(key);
+//           }
+//         })
+//       );
+//     })
+//   );
+//   self.clients.claim();
+// });
+
+// self.addEventListener("fetch", function (ev) {
+//   if (ev.request.url.includes("/api/")) {
+//     ev.respondWith(
+//       caches
+//         .open(DATA_CACHE_NAME)
+//         .then((cache) => {
+//           return fetch(ev.request)
+//             .then((response) => {
+//               // If the response was good, clone it and store it in the cache.
+//               if (response.status === 200) {
+//                 cache.put(ev.request.url, response.clone());
+//               }
+
+//               return response;
+//             })
+//             .catch((err) => {
+//               console.log(err);
+//               return cache.match(ev.request);
+//             });
+//         })
+//         .catch((err) => console.log(err))
+//     );
+
+//     return;
+//   }
+
+//   ev.respondWith(
+//     caches.match(ev.request).then(function (response) {
+//       return response || fetch(ev.request);
+//     })
+//   );
+// });
